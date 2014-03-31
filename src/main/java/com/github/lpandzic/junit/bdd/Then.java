5b8b2d44f62c874ca842da86f27f97c28158c94a
Lovro Pandzic <lovro.pandzic@gmail.com>
2014-03-31 22:00:35 +0200

    - simplification of Then - added fest assert test dependency

--- src/main/java/com/github/lpandzic/junit/bdd/Then.java
+++ src/main/java/com/github/lpandzic/junit/bdd/Then.java
@@ -1,11 +1,8 @@
 package com.github.lpandzic.junit.bdd;
 
-import org.hamcrest.Matcher;
-
 import java.util.Objects;
 import java.util.Optional;
-
-import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.function.Consumer;
 
 /**
  * @author Lovro Pandzic
@@ -22,76 +19,15 @@ public final class Then {
         }
 
         @SuppressWarnings("unchecked")
-        public MessageOrCause thenShouldThrowExceptionThat(Matcher<? extends Exception> matcher) {
+        public <R extends Throwable> void then(Consumer<R> consumer) {
 
-            Objects.requireNonNull(matcher);
-            assertThat(exception.orElse(null), (Matcher<? super T>) matcher);
-            return new MessageOrCause();
+            consumer.accept((R) exception.orElse(null));
         }
 
         @SuppressWarnings("unchecked")
-        public MessageOrCause thenShouldThrowCheckedExceptionThat(Matcher<? extends T> matcher) {
+        public <E extends T> void thenChecked(Consumer<E> consumer) {
 
-            Objects.requireNonNull(matcher);
-            assertThat(exception.orElse(null), (Matcher<? super T>) matcher);
-            return new MessageOrCause();
-        }
-
-        @SuppressWarnings("unchecked")
-        private void verifyCause(Matcher<? extends Throwable> matcher) {
-
-            Objects.requireNonNull(matcher);
-            assertThat(exception.map(T::getCause).orElse(null), (Matcher<? super Throwable>) matcher);
-        }
-
-        private void verifyMessage(Matcher<String> matcher) {
-
-            Objects.requireNonNull(matcher);
-            assertThat(exception.map(T::getMessage).orElse(null), matcher);
-        }
-
-        public final class MessageOrCause {
-
-            public CauseEnding withMessageThat(Matcher<String> matcher) {
-
-                verifyMessage(matcher);
-                return new CauseEnding();
-            }
-
-
-            public MessageEnding withCauseThat(Matcher<? extends Throwable> matcher) {
-
-                verifyCause(matcher);
-                return new MessageEnding();
-            }
-
-            private MessageOrCause() {
-
-            }
-        }
-
-        public final class CauseEnding {
-
-            public void andCauseThat(Matcher<? extends Throwable> matcher) {
-
-                verifyCause(matcher);
-            }
-
-            private CauseEnding() {
-
-            }
-        }
-
-        public final class MessageEnding {
-
-            public void andMessageThat(Matcher<String> matcher) {
-
-                verifyMessage(matcher);
-            }
-
-            private MessageEnding() {
-
-            }
+            consumer.accept((E) exception.orElse(null));
         }
     }
 
@@ -104,10 +40,9 @@ public final class Then {
             this.value = value;
         }
 
-        public void thenShouldReturnValueThat(Matcher<T> matcher) {
+        public void then(Consumer<T> consumer) {
 
-            Objects.requireNonNull(matcher);
-            assertThat(value.orElse(null), matcher);
+            consumer.accept(value.orElse(null));
         }
     }
 }
--- src/test/java/com/github/lpandzic/junit/bdd/BddTest.java
+++ src/test/java/com/github/lpandzic/junit/bdd/BddTest.java
@@ -1,6 +1,5 @@
 package com.github.lpandzic.junit.bdd;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -9,6 +8,7 @@ import java.io.IOException;
 
 import static com.github.lpandzic.junit.bdd.Bdd.when;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.Is.isA;
 
@@ -24,13 +24,14 @@ public class BddTest {
     @Test
     public void shouldPassWhenNoExceptionIsThrown() throws Exception {
 
-        when(() -> classUnderTest.returnsA(null)).thenShouldThrowExceptionThat(is(CoreMatchers.nullValue(Exception.class)));
+        when(() -> classUnderTest.returnsA(null)).then((NullPointerException e) -> assertThat(e,
+                                                                                              isA((NullPointerException.class))));
     }
 
     @Test
     public void shouldPassWhenExpectedExceptionIsThrown() throws Exception {
 
-        when(() -> classUnderTest.throwsA(new Exception())).thenShouldThrowCheckedExceptionThat(isA(Exception.class));
+        when(() -> classUnderTest.throwsA(new Exception())).thenChecked(e -> assertThat(e, isA(Exception.class)));
     }
 
     @Test
@@ -39,54 +40,15 @@ public class BddTest {
         expectedException.handleAssertionErrors();
         expectedException.expect(AssertionError.class);
 
-        when(() -> classUnderTest.throwsA(new IOException())).thenShouldThrowExceptionThat(isA(NullPointerException.class));
-    }
-
-    @Test
-    public void shouldPassWhenExceptionWithCorrectMessageIsThrown() throws Exception {
-
-        when(() -> classUnderTest.throwsA(new Exception("abc"))).thenShouldThrowCheckedExceptionThat(isA(Exception.class)).withMessageThat(is(equalTo("abc")));
-    }
-
-    @Test
-    public void shouldFailWhenExceptionWithWrongMessageIsThrown() throws Exception {
-
-        expectedException.handleAssertionErrors();
-        expectedException.expect(AssertionError.class);
-
-        when(() -> classUnderTest.throwsA(new Exception("abc"))).thenShouldThrowCheckedExceptionThat(isA(Exception.class)).withMessageThat(is(equalTo("def")));
-    }
-
-    @Test
-    public void shouldPassWhenExceptionWithCorrectCauseIsThrown() throws Exception {
-
-        when(() -> classUnderTest.throwsA(new Exception(new NullPointerException()))).thenShouldThrowCheckedExceptionThat(isA(Exception.class))
-                                                                                     .withCauseThat(isA(NullPointerException.class));
-    }
-
-    @Test
-    public void shouldFailWhenExceptionWithIncorrectCauseIsThrown() throws Exception {
-
-        expectedException.handleAssertionErrors();
-        expectedException.expect(AssertionError.class);
-
-        when(() -> classUnderTest.throwsA(new Exception(new IllegalArgumentException()))).thenShouldThrowCheckedExceptionThat(isA(Exception.class))
-                                                                                         .withCauseThat(isA(NullPointerException.class));
-    }
-
-    @Test
-    public void shouldPassWhenExceptionWithCorrectMessageAndCauseIsThrown() throws Exception {
-
-        when(() -> classUnderTest.throwsA(new Exception("abc", new NullPointerException()))).thenShouldThrowCheckedExceptionThat(isA(Exception.class))
-                                                                                            .withMessageThat(is(equalTo("abc")))
-                                                                                            .andCauseThat(isA(NullPointerException.class));
+        when(() -> classUnderTest.throwsA(new IOException())).then((NullPointerException e) -> assertThat(e,
+                                                                                                          isA(NullPointerException.class)));
     }
 
     @Test
     public void shouldPassWhenReturnsExpectedObject() {
 
         Object value = new Object();
-        when(classUnderTest.returnsA(value)).thenShouldReturnValueThat(is(equalTo(value)));
+        when(classUnderTest.returnsA(value)).then(actual -> assertThat(actual, is(equalTo(value))));
     }
 
     @Test
@@ -94,13 +56,13 @@ public class BddTest {
 
         when(() -> {
             throw new Exception();
-        }).thenShouldThrowCheckedExceptionThat(isA(Exception.class));
+        }).thenChecked(e -> assertThat(e, isA(Exception.class)));
     }
 
     @Test
     public void simpleLambdaReturnTest() {
 
-        when(new Object()).thenShouldReturnValueThat(isA(Object.class));
+        when(new Object()).then(actual -> assertThat(actual, isA(Object.class)));
     }
 
     @Test
@@ -112,13 +74,13 @@ public class BddTest {
         expectedException.expectMessage("but: was");
 
         Object value = new Object();
-        when(classUnderTest.returnsA(value)).thenShouldReturnValueThat(is(equalTo(new Object())));
+        when(classUnderTest.returnsA(value)).then(actual -> assertThat(actual, is(equalTo(new Object()))));
     }
 
     @Test
     public void shouldPassWhenReturnsExpectedNull() {
 
-        when(classUnderTest.<Object>returnsA(null)).thenShouldReturnValueThat(is(equalTo(null)));
+        when(classUnderTest.returnsA(new Object())).then(actual -> assertThat(actual, is(equalTo(null))));
     }
 
     private static class ClassUnderTest {
